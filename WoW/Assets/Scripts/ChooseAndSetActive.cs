ba2f9a7e15b610942e92b9e6c7736797caa49625
jvdwijk <joeyvdwijk@gmail.com>
2016-12-20 09:56:03 +0100

    First Commit

---
+++ WoW/Assets/Scripts/ChooseAndSetActive.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChooseAndSetActive : MonoBehaviour
+{
+
+    [SerializeField]
+	private GameObject[] mainAsset; //always put in the main asset first!
+
+    void Awake()
+    {
+		for (int i = 0; i < mainAsset.Length; i++)
+        {
+			mainAsset[i] = mainAsset[i].GetComponent<GameObject>();
+			if(mainAsset[i] != mainAsset[0]) 
+            {
+				mainAsset[i].SetActive (false);
+            }
+            else
+            {
+				mainAsset[i].SetActive (true);
+            }
+        }
+    }
+
+    public void ScreenUpdate(Canvas menu)
+    {
+		for (int i = 0; i < mainAsset.Length; i++)
+        {
+			if (mainAsset[i].name == menu.name)
+            {
+				mainAsset[i].SetActive (true);
+            }
+            else
+            {
+				mainAsset[i].SetActive (false);
+            }
+        }       
+    }
+}
