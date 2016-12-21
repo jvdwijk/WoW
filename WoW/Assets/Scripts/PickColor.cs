3a41b5d1a57dc5d7e123d207d43a73ebd43755c4
jvdwijk <joeyvdwijk@gmail.com>
2016-12-21 13:24:42 +0100

    Pick Color start

---
+++ WoW/Assets/Scripts/PickColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickColor : MonoBehaviour {
+	private Renderer render;
+	[SerializeField]
+	private Color[] color;
+
+	public void PutColor (int chosenColor) {
+		render = GetComponent<Renderer> ();
+		render.material.SetColor ("_Color", color[chosenColor - 1]);
+	}
+}
