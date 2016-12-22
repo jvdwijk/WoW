1bda499ba9bdfb0d5e7bfa86f87dbc8911bf4435
sebastiaan <sebastiaanvoebal@hotmail.com>
2016-12-22 10:56:47 +0100

    camera

---
+++ WoW/Assets/RotateObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+    private float horizontalSpeed = 2.0F;
+    public GameObject leftButton;
+    public GameObject rightbutton;
+    
+    public void Rotate()
+    {
+        transform.Rotate(Vector3.up, horizontalSpeed * Time.deltaTime);
+
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            transform.Rotate(0, -h, 0);
+        }
+
+     }
+}
