d0f4eed7de49d56919cf5c864bf1063faf6fc964
Jelmer Le Mair <jelmerlm1@gmail.com>
2016-12-22 11:39:13 +0100

    alles is klaar en ui update. was hair style button vergeten .-.
    boosted bonobo

---
+++ WoW/Assets/Scripts/Alles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Alles");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Alles");
+	}
+}
