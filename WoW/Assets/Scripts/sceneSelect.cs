6ff12dc29b56f8b1b29e3244590b85d49a6b9b01
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-01-10 16:15:39 +0100

    scene select

---
+++ WoW/Assets/Scripts/sceneSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class sceneSelect: MonoBehaviour
+{
+    public void SceneSelect(int scene)
+    {
+
+        SceneManager.LoadScene(scene);
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("heor");
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
