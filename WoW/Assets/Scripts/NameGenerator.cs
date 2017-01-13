16dc90601dc2ed31536079d57f44fccd88da2f72
jvdwijk <joeyvdwijk@gmail.com>
2017-01-13 11:47:02 +0100

    Random Name Gen, Color fix

---
+++ WoW/Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NameGenerator : MonoBehaviour {
+	[SerializeField]
+	private string[] names;
+	[SerializeField]
+	private Text nameSpace;
+
+	public void ChangeName()
+	{
+		var tempNumber = Random.Range (0, names.Length);
+
+		for (int i = 0; i < names.Length; i++) {
+			if (tempNumber == i){
+				PlayerPrefs.SetString ("Name",names[i]);
+				nameSpace.text = names [i];
+			}
+		}
+	}
+}
