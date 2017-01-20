643fb566b852acab2064ab2d00a0ca76c0a3a18a
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-01-20 12:07:02 +0100

    hold button

--- WoW/Assets/Scripts/RotateObject.cs
+++ WoW/Assets/Scripts/RotateObject.cs
@@ -5,24 +5,46 @@ public class RotateObject : MonoBehaviour
 {
     private float horizontalSpeed = 2.0F;
     public float turnSpeed = 200f;
-    
-   
+    private bool _rotate;
+    private bool lrotate;
     void Update()
     {
+        if (lrotate)
+        {
+            transform.Rotate(new Vector3(0, 5, 0));
+
+        }
+        if (_rotate)
+        {
+            transform.Rotate(new Vector3(0, -5, 0));
+        }
         if (Input.GetMouseButton(0))
         {
             float h = horizontalSpeed * Input.GetAxis("Mouse X");
             transform.Rotate(0, -h, 0);
         }
+    }
+         public void OnPress()
+    {
+        _rotate = true;
+    }
 
-    }
-    public void RotateRight()
+    public void OnRelease()
     {
-        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        _rotate = false;
     }
-    public void RotateLeft()
+    public void lOnPress()
     {
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        lrotate = true;
     }
-    
+
+    public void lOnRelease()
+    {
+        lrotate = false;
+    }
+
+
 }
+
+
+
