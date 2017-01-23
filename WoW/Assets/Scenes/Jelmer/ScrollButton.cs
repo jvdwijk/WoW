238ecb82b0f53f2aa3df5b5fb76a9861284f7500
Jelmer Le Mair <jelmerlm1@gmail.com>
2017-01-23 13:43:28 +0100

    ui stuff

---
+++ WoW/Assets/Scenes/Jelmer/ScrollButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollButton : MonoBehaviour
+{
+
+    float buttonPosition;
+
+    void Start() {
+        buttonPosition = GameObject.Find("Content").transform.position.y;
+        Debug.Log(buttonPosition);
+                }
+    void Update()
+    {
+        buttonPosition = GameObject.Find("Content").transform.position.y;
+    }
+
+    public void UpButton()
+    {
+        buttonPosition += 10;
+        Debug.Log(buttonPosition);
+    }
+
+}
