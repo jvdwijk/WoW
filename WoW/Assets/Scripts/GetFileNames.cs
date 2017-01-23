3e6aed4db0199617721ea845d8e7d5b57e9b2f24
jvdwijk <joeyvdwijk@gmail.com>
2017-01-23 17:22:34 +0100

    start getFileNames

---
+++ WoW/Assets/Scripts/GetFileNames.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFileNames : MonoBehaviour {
+
+	void Awake () {
+		foreach(string file in System.IO.Directory.GetFiles(Application.persistentDataPath))
+        {
+        }
+	}
+}
