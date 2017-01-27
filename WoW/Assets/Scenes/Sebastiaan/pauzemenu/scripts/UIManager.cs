f207c565c48f2583db842df11faa113f692f0419
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-01-27 22:09:08 +0100

    pauze menu

---
+++ WoW/Assets/Scenes/Sebastiaan/pauzemenu/scripts/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+    public GameObject pausepannel;
+
+    public bool isPaused;
+
+	
+	void Start () {
+        isPaused = false;
+	}
+	
+	
+	void Update () {
+	if (isPaused)
+        {
+            PauseGame(true);
+        }
+        else
+        {
+            PauseGame(false);
+        }
+    if(Input.GetButtonDown("Cancel"))
+        {
+            SwitchPause();
+
+        }
+
+	}
+
+
+    void PauseGame (bool state)
+    {
+        if (state)
+        {
+            
+            Time.timeScale = 0.0f; //pauze
+        }
+        else
+        {
+            Time.timeScale = 1.0f; //play
+           }
+        pausepannel.SetActive(state);
+        }
+    public void SwitchPause()
+    {
+        if (isPaused) {
+            isPaused = false; //changes the value
+        }
+        else{
+            isPaused = true;
+
+        }
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Resume()
+    {
+
+        SwitchPause();
+    }
+}
