905408f658fc181c4d388e7f5c575b8c4523a0c5
jvdwijk <joeyvdwijk@gmail.com>
2017-01-27 10:09:28 +0100

    scripts improvements, scene work

---
+++ WoW/Assets/Scripts/Save.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+
+public class Save : MonoBehaviour {
+
+    public void Saving(object usedClass)
+    {
+        var serializer = new XmlSerializer(typeof(NewPlayer));
+        var filePath = Application.persistentDataPath + "/" + "Characters" + ".xml";
+
+        FileMode currentFileMode = (File.Exists(filePath)) ? FileMode.Append : FileMode.Create;
+
+        FileStream fileStream = File.Open(Application.persistentDataPath + "/" + "Characters" + ".xml", currentFileMode);
+
+        serializer.Serialize(fileStream, usedClass);
+        fileStream.Close();
+    }
+}
--- WoW/Assets/Scripts/SaveToFile.cs
+++ WoW/Assets/Scripts/SaveToFile.cs
@@ -1,53 +1,29 @@
-﻿using System.Collections;
-using System;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-
 using System.Xml;
 using System.Xml.Serialization;
 
-public class SaveToFile : MonoBehaviour {
+public class SaveToFile : Save {
 	
 	public void WriteToFile()
 	{
-        Debug.Log(PlayerPrefs.GetInt("HairType") + " " + PlayerPrefs.GetInt("HairType"));
-        //var bf = new BinaryFormatter();
-        
+        Debug.Log(PlayerPrefs.GetInt("HairType") + " " + PlayerPrefs.GetInt("HairType"));       
 		var data = new NewPlayer ();
 		data.playerName = PlayerPrefs.GetString ("Name");
 		data.hairType = PlayerPrefs.GetInt("HairType");
 		data.hairColor = PlayerPrefs.GetInt("HairColor");
 
-        data.Save();
+        Saving(data);
 	}
 }
 
 [Serializable] [XmlRoot("PlayerCollection")]
 public class NewPlayer
 {
-   // [XmlArray("Players")[XmlArrayItem("Player")]
     [XmlAttribute("name")]
     public string playerName;
     [XmlAttribute("hairType")]
 	public int hairType;
     [XmlAttribute("hairColor")]
 	public int hairColor;
-
-    public void Save()
-    {
-        var serializer = new XmlSerializer(typeof(NewPlayer));
-        FileStream fileStream;
-        if (File.Exists(Application.persistentDataPath + "/" + "Characters" + ".xml"))
-        {
-            fileStream = File.Open(Application.persistentDataPath + "/" + "Characters" + ".xml", FileMode.Append);
-        }
-        else
-        {
-            fileStream = File.Open(Application.persistentDataPath + "/" + "Characters" + ".xml", FileMode.Create);
-        }
-        serializer.Serialize(fileStream, this);
-        fileStream.Close();
-    }
 }
