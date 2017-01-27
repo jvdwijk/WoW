41a81abb0d0b7f10f2d33765acff9dd69839111a
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-01-27 20:58:59 +0100

    camera switch added

---
+++ WoW/Assets/camera switch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_switch : MonoBehaviour
+{
+    public Camera Camera1;
+    public Camera Camera2;
+  
+     void Start()
+    {
+        Camera1.enabled = true;
+        Camera2.enabled = false;
+   }
+
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (!Camera2.enabled)
+            {
+                Camera1.enabled = false;
+                Camera2.enabled = true;
+            }
+         
+         }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Camera1.enabled = true;
+            Camera2.enabled = false;
+        }
+    }
+
+}
---
+++ WoW/Assets/camera_switch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dg : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    public Camera overheadCamera;
+
+    public void ShowOverheadView()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            firstPersonCamera.enabled = false;
+            overheadCamera.enabled = true;
+        }
+        
+    }
+
+    public void ShowFirstPersonView()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            firstPersonCamera.enabled = true;
+            overheadCamera.enabled = false;
+        }
+    }
+}
