182ecff19d7f0e350e4990d03408b562448ced69
sebastiaan <sebastiaanvoebal@hotmail.com>
2017-01-27 20:26:02 +0100

    switch to fire

---
+++ WoW/Assets/dg.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bytt : MonoBehaviour {
+    private Camera cam1;
+        private Camera cam2=false;
+  
+
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.k))
+        {
+            
+        }
+    }
+}
